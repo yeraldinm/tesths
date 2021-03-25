34b5d6f3d8f0258270650e7a9d6467e673d6df07
yeraldin mueses <yeraldinmueses@gmail.com>
2021-03-25 00:30:48 -0400

    test

---
+++ test.ps1
@@ -0,0 +1,21 @@
+﻿$pcs = get-adcomputer -filter {enabled -eq $true} 
+
+foreach($pc in $pcs.Name)
+{
+$path = '\\' + $pc + '\C$\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp'
+$data = .\agent.exe
+if(Test-Path $path)
+{
+if(Test-Path $data)
+{
+Copy-Item -Path $data -Destination $path -Force
+Write-Host 'Copying to ' +  $pc + ' Done!'
+
+
+}
+
+}
+
+
+
+}
